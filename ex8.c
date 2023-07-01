099caf0b8ef2c65dc1c398191d745084f426a1de
Yasmin <souzasmimi@gmail.com>
2023-07-01 13:52:16 -0300

    alt_8

--- ex8.c
+++ ex8.c
@@ -7,10 +7,16 @@ Venus 108 -150.5 485
 Marte 228 -142 17
 J´upiter 778 -240 12
 Crie uma estrutura Planeta para armazenar os dados e fa¸ca a leitura desse arquivo chamado
-“planetas.txt”*/
+“planetas.txt”
+criar 2 funçoes:
+1. função que gravar dados no arquivo
+2. função que leia os dados do arquivo e imprima na tela
+*/
+
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char nome[20];
@@ -19,38 +25,61 @@ typedef struct {
     float temp_max;
 } Planeta;
 
-void gravar_dados() {
-    Planeta p;
+void gravarDados(Planeta *p, int n) {
     FILE *arq;
-    arq = fopen("planetas.txt", "a");
-    if (arq == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        exit(1);
+    int i;
+
+    arq = fopen("planetas.txt", "w");
+
+    for (i = 0; i < n; i++) {
+        fprintf(arq, "%s %.2f %.2f %.2f\n", p[i].nome, p[i].distancia, p[i].temp_min, p[i].temp_max);
     }
-    printf("Digite o nome do planeta: ");
-    scanf("%s", p.nome);
-    printf("Digite a distancia do planeta: ");
-    scanf("%f", &p.distancia);
-    printf("Digite a temperatura minima do planeta: ");
-    scanf("%f", &p.temp_min);
-    printf("Digite a temperatura maxima do planeta: ");
-    scanf("%f", &p.temp_max);
-    fprintf(arq, "%s %f %f %f\n", p.nome, p.distancia, p.temp_min, p.temp_max);
+
+    fclose(arq);
+}
+
+void lerDados(Planeta *p, int n) {
+    FILE *arq;
+    int i;
+
+    arq = fopen("planetas.txt", "r");
+
+    for (i = 0; i < n; i++) {
+        fscanf(arq, "%s %f %f %f", p[i].nome, &p[i].distancia, &p[i].temp_min, &p[i].temp_max);
+    }
+
     fclose(arq);
 }
 
 int main() {
-    Planeta p;
-    FILE *arq;
-    gravar_dados();
-    arq = fopen("planetas.txt", "r");
-    if (arq == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        exit(1);
+    Planeta *p;
+    int n, i;
+
+    printf("Digite o numero de planetas: ");
+    scanf("%d", &n);
+
+    p = (Planeta *) malloc(n * sizeof(Planeta));
+
+    for (i = 0; i < n; i++) {
+        printf("Digite o nome do planeta: ");
+        scanf("%s", p[i].nome);
+        printf("Digite a distancia do planeta: ");
+        scanf("%f", &p[i].distancia);
+        printf("Digite a temperatura minima do planeta: ");
+        scanf("%f", &p[i].temp_min);
+        printf("Digite a temperatura maxima do planeta: ");
+        scanf("%f", &p[i].temp_max);
     }
-    while (fscanf(arq, "%s %f %f %f", p.nome, &p.distancia, &p.temp_min, &p.temp_max) == 4) {
-        printf("%s %f %f %f\n", p.nome, p.distancia, p.temp_min, p.temp_max);
+
+    gravarDados(p, n);
+
+    lerDados(p, n);
+
+    for (i = 0; i < n; i++) {
+        printf("%s %.2f %.2f %.2f\n", p[i].nome, p[i].distancia, p[i].temp_min, p[i].temp_max);
     }
-    fclose(arq);
+
+    free(p);
+
     return 0;
 }
